d38db61117aec9f51bc77234c667861f4a328d3f
PriyanshPal <priyanshp21775@gmail.com>
2023-03-19 12:13:06 +0530

    added checkForVictory

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import com.example.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         NOUGHT,
         CROSS
     }
+
+    private var crossesScore = 0
+    private var noughtsScore = 0
 
     companion object {
         const val NOUGHT = "O"
@@ -48,6 +52,80 @@ class MainActivity : AppCompatActivity() {
             return
         }
         addToBoard(view)
+
+        if(checkForVictory(NOUGHT)) {
+            noughtsScore++
+            result("Noughts win!!")
+        }
+
+        if(checkForVictory(CROSS)) {
+            crossesScore++
+            result("Crosses win!!")
+        }
+
+        if(fullBoard()) {
+            result("draw")
+        }
+    }
+
+    private fun checkForVictory(s: String): Boolean {
+        //HORIZONTAL VICTORY
+        if(match(binding.a1, s) && match(binding.a2, s) && match(binding.a3, s))
+            return true
+        if(match(binding.b1, s) && match(binding.b2, s) && match(binding.b3, s))
+            return true
+        if(match(binding.c1, s) && match(binding.c2, s) && match(binding.c3, s))
+            return true
+
+        //VERTICLE VICTORY
+        if(match(binding.a1, s) && match(binding.b1, s) && match(binding.c1, s))
+            return true
+        if(match(binding.a2, s) && match(binding.b2, s) && match(binding.c2, s))
+            return true
+        if(match(binding.a3, s) && match(binding.b3, s) && match(binding.c3, s))
+            return true
+
+        //DIAGONAL VICTORY
+        if(match(binding.a1, s) && match(binding.b2, s) && match(binding.c3, s))
+            return true
+        if(match(binding.a3, s) && match(binding.b2, s) && match(binding.c1, s))
+            return true
+        return false
+    }
+
+    private fun match(button: Button, symbol: String) : Boolean = button.text == symbol
+
+    private fun result(title: String) {
+        val message = "\nNoughts $noughtsScore \n \nCrosses $crossesScore"
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Reset")
+            { _,_ ->
+                resetBoard()
+            }
+            .setCancelable(false)
+            .show()
+    }
+
+    private fun resetBoard() {
+        for(button in boardList) {
+            button.text = ""
+        }
+        if(firstTurn == Turn.NOUGHT)
+            firstTurn = Turn.CROSS
+        else if(firstTurn == Turn.CROSS)
+            firstTurn = Turn.NOUGHT
+        currentTurn = firstTurn
+        setTurnLabel()
+    }
+
+    private fun fullBoard(): Boolean {
+        for(button in boardList) {
+            if(button.text == "")
+                return false
+        }
+        return true
     }
 
     private fun addToBoard(button: Button) {
